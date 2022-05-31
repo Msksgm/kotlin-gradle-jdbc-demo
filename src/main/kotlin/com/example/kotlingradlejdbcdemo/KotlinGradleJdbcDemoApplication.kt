06d50660a5939a1ba3b13d770667b20d4dff78f9
Msksgm <msksgm3124@gmail.com>
2022-06-01 07:37:13 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlingradlejdbcdemo/KotlinGradleJdbcDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlingradlejdbcdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinGradleJdbcDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinGradleJdbcDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlingradlejdbcdemo/KotlinGradleJdbcDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlingradlejdbcdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinGradleJdbcDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
