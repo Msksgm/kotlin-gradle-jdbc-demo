8113e37a9aa19178ca69776e189a30aa20e1a7fa
Msksgm <msksgm3124@gmail.com>
2022-06-02 07:55:40 +0900

    add: lint

--- src/main/kotlin/com/example/kotlingradlejdbcdemo/KotlinGradleJdbcDemoApplication.kt
+++ src/main/kotlin/com/example/kotlingradlejdbcdemo/KotlinGradleJdbcDemoApplication.kt
@@ -1,11 +1,29 @@
 package com.example.kotlingradlejdbcdemo
 
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.jdbc.core.JdbcTemplate
 
 @SpringBootApplication
-class KotlinGradleJdbcDemoApplication
+class KotlinGradleJdbcDemoApplication(val jdbcTemplate: JdbcTemplate) : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        println("--Create--")
+        InsertCommandImpl(jdbcTemplate).perform("Carol", "Sample3")
+        println("--Read--")
+        SelectAllQueryImpl(jdbcTemplate).perform().forEach { println(it) }
+        println("--Update--")
+        UpdateCommandImpl(jdbcTemplate).perform("Sample3")
+        println("--Read--")
+        SelectAllQueryImpl(jdbcTemplate).perform().forEach { println(it) }
+        println("--Delete--")
+        DeleteCommandImpl(jdbcTemplate).perform("Eve")
+        println("--Read--")
+        SelectAllQueryImpl(jdbcTemplate).perform().forEach { println(it) }
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<KotlinGradleJdbcDemoApplication>(*args)
+    runApplication<KotlinGradleJdbcDemoApplication>(*args)
 }
--- src/test/kotlin/com/example/kotlingradlejdbcdemo/KotlinGradleJdbcDemoApplicationTests.kt
+++ src/test/kotlin/com/example/kotlingradlejdbcdemo/KotlinGradleJdbcDemoApplicationTests.kt
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class KotlinGradleJdbcDemoApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
